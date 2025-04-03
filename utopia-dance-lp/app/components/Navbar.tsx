045f80afe743ecab4e19bd4efa7de039c4810b75
yusukekikuta0509 <yusukekikuta.05@gmail.com>
2025-04-03 22:58:57 +0900

    Update Navbar.tsx

--- utopia-dance-lp/app/components/Navbar.tsx
+++ utopia-dance-lp/app/components/Navbar.tsx
@@ -333,14 +333,8 @@ export default function Navbar() {
                         style={{ color: '#fff' }}
                         onClick={() => setIsOpen(false)}
                     >
-                        <svg width="32" height="32" fill="none" stroke="white" viewBox="0 0 24 24" aria-hidden="true">
-                        <path 
-                            d="M6 18L18 6M6 6l12 12" 
-                            stroke="white" 
-                            strokeWidth={2} 
-                            strokeLinecap="round" 
-                            strokeLinejoin="round" 
-                        />
+                        <svg width="32" height="32" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="white"/>
                         </svg>
                     </Link>
                     
